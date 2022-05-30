925eab10faf1f931445b3c1c95f06369f95803ec
First Last <storonskyi.roman@gmail.com>
2022-05-30 12:50:54 +0300

    JS update

--- force-app/main/default/lwc/accountWeather/accountWeather.js
+++ force-app/main/default/lwc/accountWeather/accountWeather.js
@@ -1,34 +1,47 @@
 import { LightningElement , api, wire, track } from 'lwc';
 import getWeather from '@salesforce/apex/AccountWeatherController.getWeather';
-import getWeatherImage from '@salesforce/apex/AccountWeatherController.getWeather';
+import getWeatherImage from '@salesforce/apex/AccountWeatherController.getWeatherImage';
+import AccountId from '@salesforce/schema/Case.AccountId';
 
 
 export default class AccountWeather extends LightningElement {
-
+    @track error;
     @track weather = {};
-    @track weatherImage;
+    @track weatherImage = '';
     @api recordId;
     
-    // @wire(getWeatherImage, {accId: '$weather.main__c' })
-    // getWeatherImage({data, error}){
-    //     if(data){
-    //         console.log(data);
-    //         this.weatherImage = data;
-    //     }
-    // }
-    
-
-    @wire(getWeather, {accId: '$recordId' })
-    getWeather({data, error}){
-        if(data){
-            console.log(data);
-            this.weather = data;
-        }
+    connectedCallback(){
+        getWeather({accId : this.recordId })
+        .then(result => {
+            console.log('accId' + result);
+            this.weather = result;
+            this.error = undefined;
+            console.log('this.weather.main__c ' + JSON.stringify(this.weather));
+            getWeatherImage({weatherMain: this.weather.Main__c })
+                .then(result => {
+                    console.log('weatherMain' + result);
+                    this.weatherImage = result;
+                    this.error = undefined;
+                })
+                .catch(error => {
+                    this.error = error;
+                    this.weather = undefined;
+                });
+        })
+        .catch(error => {
+            this.error = error;
+            this.weather = undefined;
+        });
     }
 
-    
 
-    //get Weather record for this city
-
-    //display the Weather record
+    // @wire(getWeather, {accId: '$recordId' })
+    // getWeather({data, error}){
+    //     if(data){
+    //         console.log(data);
+    //         this.weather = data;
+    //         console.log(data.main__c);
+    //         this.weatherImage = data.main__c;
+    //     }
+    // }
 }
