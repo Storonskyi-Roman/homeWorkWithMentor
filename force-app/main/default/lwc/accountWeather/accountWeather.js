0d0e07dc7dc309c27e567e471bbaf916fe7026ee
First Last <storonskyi.roman@gmail.com>
2022-05-31 11:17:33 +0300

    use custom metadata and @wire in js

--- force-app/main/default/lwc/accountWeather/accountWeather.js
+++ force-app/main/default/lwc/accountWeather/accountWeather.js
@@ -1,47 +1,55 @@
 import { LightningElement , api, wire, track } from 'lwc';
 import getWeather from '@salesforce/apex/AccountWeatherController.getWeather';
 import getWeatherImage from '@salesforce/apex/AccountWeatherController.getWeatherImage';
-import AccountId from '@salesforce/schema/Case.AccountId';
 
 
 export default class AccountWeather extends LightningElement {
+
     @track error;
     @track weather = {};
-    @track weatherImage = '';
+    @track weatherImage;
     @api recordId;
-    
-    connectedCallback(){
-        getWeather({accId : this.recordId })
-        .then(result => {
-            console.log('accId' + result);
-            this.weather = result;
-            this.error = undefined;
-            console.log('this.weather.main__c ' + JSON.stringify(this.weather));
-            getWeatherImage({weatherMain: this.weather.Main__c })
-                .then(result => {
-                    console.log('weatherMain' + result);
-                    this.weatherImage = result;
-                    this.error = undefined;
-                })
-                .catch(error => {
-                    this.error = error;
-                    this.weather = undefined;
-                });
-        })
-        .catch(error => {
-            this.error = error;
-            this.weather = undefined;
-        });
+
+    @wire(getWeather, {accId: '$recordId' })
+    getWeather({data, error}){
+        if(data){
+            console.log(data);
+            this.weather = data;
+            console.log(this.weather.Main__c);
+        }
     }
 
-
-    // @wire(getWeather, {accId: '$recordId' })
-    // getWeather({data, error}){
-    //     if(data){
-    //         console.log(data);
-    //         this.weather = data;
-    //         console.log(data.main__c);
-    //         this.weatherImage = data.main__c;
-    //     }
+    @wire(getWeatherImage, {weatherMain: '$weather.Main__c' })
+    getWeatherImage({data, error}){
+        if(data){
+            console.log(data);
+            this.weatherImage = data;
+        }
+    }
+    // connectedCallback(){
+    //     getWeather({accId : this.recordId })
+    //     .then(result => {
+    //         console.log('accId' + result);
+    //         this.weather = result;
+    //         this.error = undefined;
+    //         console.log('this.weather.main__c ' + JSON.stringify(this.weather));
+    //         getWeatherImage({weatherMain: this.weather.Main__c })
+    //             .then(result => {
+    //                 console.log('weatherMain' + result);
+    //                 this.weatherImage = result;
+    //                 this.error = undefined;
+    //             })
+    //             .catch(error => {
+    //                 this.error = error;
+    //                 this.weather = undefined;
+    //             });
+    //     })
+    //     .catch(error => {
+    //         this.error = error;
+    //         this.weather = undefined;
+    //     });
     // }
+
+
+    
 }
